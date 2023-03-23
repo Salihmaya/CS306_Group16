c31467e7fb5ed077e9dc8e150988292833909df7
Berke Ceylan <berkeceylan12@hotmail.com>
2023-03-23 21:36:16 +0300

    Delete death-rate-smoking.sql

--- SQLFiles/death-rate-smoking.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE CS306_Project;
-USE CS306_Project;
-CREATE TABLE death_rate_smoking(iso_code VarChar(50),
-								year int,
-								death_rate double
-                                );
-SELECT * FROM death_rate_smoking;
