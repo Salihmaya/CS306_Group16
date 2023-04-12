909d8a9f641b4e4174a0267cac6ae3250a9d4ced
nisaerdal7 <76566903+nisaerdal7@users.noreply.github.com>
2023-04-12 22:17:03 +0300

    Update high_lung_cancer_deaths.sql

--- Views/high_lung_cancer_deaths.sql
+++ Views/high_lung_cancer_deaths.sql
@@ -1,5 +1,5 @@
 CREATE VIEW high_lung_cancer_deaths AS
-SELECT *
+SELECT country_name, year
 FROM lung_cancer_deaths
 WHERE male_death_rate > (SELECT AVG(male_death_rate) FROM lung_cancer_deaths) AND female_death_rate >(SELECT AVG(female_death_rate) FROM lung_cancer_deaths);
 
