aa9854727e6ffdca7329b2634d9aae4c8e549a9c
nisaerdal7 <76566903+nisaerdal7@users.noreply.github.com>
2023-04-19 00:11:17 +0300

    Add files via upload

---
+++ Aggregate Statements/min_advertisement_bans.sql
@@ -0,0 +1,13 @@
+-- finds the lung cancer death rates of the countries with minimum advertisement bans in a specific year.
+
+SELECT lc.iso_code, lc.year, lc.male_death_rate, lc.female_death_rate
+FROM lung_cancer_deaths lc
+WHERE (lc.iso_code, lc.year) IN (
+  SELECT ca.iso_code, ca.year
+  FROM cigarette_advertisements ca
+  GROUP BY ca.iso_code, ca.year
+  HAVING MIN(ca.ban_indicator) = (
+    SELECT MIN(ban_indicator)
+    FROM cigarette_advertisements
+  )
+);
