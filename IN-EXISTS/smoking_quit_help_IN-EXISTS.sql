20f2c8abc20d812746700782a50e20ce3c15a9e9
Salihmaya <101068424+Salihmaya@users.noreply.github.com>
2023-04-22 14:47:14 +0300

    Create smoking_quit_help_IN-EXISTS.sql

---
+++ IN-EXISTS/smoking_quit_help_IN-EXISTS.sql
@@ -0,0 +1,15 @@
+
+Select *
+FROM high_smoke_quit_help
+Where (country_name, year) IN
+(SELECT country_name,year
+FROM low_death_rate_smoking);
+
+SELECT *
+FROM high_smoke_quit_help q
+WHERE EXISTS (
+  SELECT country_name, year
+  FROM low_death_rate_smoking d
+  WHERE q.country_name = d.country_name
+  AND q.year = d.year
+);
