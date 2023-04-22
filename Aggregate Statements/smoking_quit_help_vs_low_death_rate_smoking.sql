72292bdbf8f87c57daa47f6ffe819bf34edfd683
Salihmaya <101068424+Salihmaya@users.noreply.github.com>
2023-04-22 23:29:14 +0300

    Update smoking_quit_help_vs_low_death_rate_smoking.sql

--- Aggregate Statements/smoking_quit_help_vs_low_death_rate_smoking.sql
+++ Aggregate Statements/smoking_quit_help_vs_low_death_rate_smoking.sql
@@ -1,3 +1,4 @@
+-- AVG Statement was used to select the help indicator values bigger than the average
 SELECT q.iso_code, q.year, AVG(q.help_indicator) AS avg_help_indicator
 FROM smoking_quit_help q
 LEFT JOIN low_death_rate_smoking d ON q.iso_code = (SELECT iso_code FROM countries WHERE country_name = d.country_name) AND q.year = d.year
