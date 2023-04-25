e01b2716c7c2054c1862079564fcb6c16b3b34a4
ismail çakmak <101394249+smalcakmak@users.noreply.github.com>
2023-04-25 12:42:25 +0300

    Add files via upload

---
+++ Stored Procedure/advertisement_stored_procedure.sql
@@ -0,0 +1,12 @@
+
+
+
+DELIMITER $$
+CREATE PROCEDURE find_countries(IN ban_indicator INT)
+BEGIN
+	SELECT *
+    FROM cigarette_advertisements c
+    WHERE c.ban_indicator = ban_indicator;
+END$$
+
+DELIMITER ;
