0d00c3ff4e0402e195a9393ed1fb73a3ef85a832
nisaerdal7 <76566903+nisaerdal7@users.noreply.github.com>
2023-05-11 23:54:22 +0300

    Add files via upload

---
+++ VisualizationCodes/lung_cancer_barch.py
@@ -0,0 +1,77 @@
+import mysql.connector
+import matplotlib.pyplot as plt
+import pandas as pd
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Cs306",
+  database="cs306"
+)
+
+mycursor = mydb.cursor()
+
+query = """
+SELECT ctr.country_name, AVG(c.ban_indicator) AS avg_ban_indicator, AVG(d.male_death_rate) AS avg_male_death_rate, AVG(d.female_death_rate) AS avg_female_death_rate, count(c.year)
+FROM cigarette_advertisements c, lung_cancer_deaths d, countries ctr
+WHERE c.iso_code = d.iso_code AND c.year = d.year AND c.iso_code = ctr.iso_code
+group by c.iso_code
+Having AVG(c.ban_indicator) >= 4.5
+ORDER BY AVG(male_death_rate)+AVG(female_death_rate) ASC
+LIMIT 10;
+"""
+
+mycursor.execute(query)
+
+data = mycursor.fetchall()
+
+# Get the column names from the cursor description
+column_names = [desc[0] for desc in mycursor.description]
+
+# Create a pandas dataframe from the data and column names
+df = pd.DataFrame(data, columns=column_names)
+
+plt.figure(figsize=(10, 6))
+plt.barh(df['country_name'], df['avg_male_death_rate'], label='Male Death Rate')
+plt.barh(df['country_name'], df['avg_female_death_rate'], label='Female Death Rate')
+
+plt.xlabel('Death Rate')
+plt.ylabel('Country Name')
+plt.title('Average Lung Cancer Death Rates of Countries with Strict Advertisement Ban Policy')
+
+# Execute the SQL query to retrieve data from the view
+query = """
+SELECT c.country_name, AVG(l.male_death_rate) as avg_male, AVG(l.female_death_rate) as avg_female
+FROM lung_cancer_deaths l
+JOIN countries c ON l.iso_code = c.iso_code
+GROUP BY c.country_name
+ORDER BY (avg_male + avg_female) DESC
+LIMIT 5;
+"""
+mycursor.execute(query)
+
+# Fetch all the data from the query result
+data = mycursor.fetchall()
+
+# Get the column names from the cursor description
+column_names = [desc[0] for desc in mycursor.description]
+
+# Create a pandas dataframe from the data and column names
+df = pd.DataFrame(data, columns=column_names)
+
+# Create pie charts for each country
+for index, row in df.iterrows():
+    country_name = row['country_name']
+    male_death_rate = row['avg_male']
+    female_death_rate = row['avg_female']
+    
+    # Create a pie chart
+    labels = ['Male Death Rate', 'Female Death Rate']
+    sizes = [male_death_rate, female_death_rate]
+    
+    plt.figure(figsize=(6, 6))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    plt.title(f'{country_name}')
+    plt.show()
+
+
