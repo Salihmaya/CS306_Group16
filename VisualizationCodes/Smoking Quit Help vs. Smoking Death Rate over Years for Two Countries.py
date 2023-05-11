1a1fbd7d0019546c355c8e9ef38a3d04e25a425f
Salihmaya <101068424+Salihmaya@users.noreply.github.com>
2023-05-12 01:11:33 +0300

    Update and rename Smoking Quit Help vs. Smoking Death Rate over Years for Two Countries to Smoking Quit Help vs. Smoking Death Rate over Years for Two Countries.py

--- VisualizationCodes/Smoking Quit Help vs. Smoking Death Rate over Years for Two Countries.py
+++ VisualizationCodes/Smoking Quit Help vs. Smoking Death Rate over Years for Two Countries.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 
 config = {
-    'user': 'root',
-    'password': 'Elazigligakkos23',
-    'host': 'localhost',
-    'database': 'sies',
+    'user': '**',
+    'password': '**',
+    'host': '**',
+    'database': '**',
 }
 
 
